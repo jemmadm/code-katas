a8b580f9e26653d86478bdbd463b9e92328da5a9
michael.hatch <michael.hatch@autotrader.co.uk>
2014-09-17 12:39:04 +0000

    [michael-ha] commiting first stage of fizz buzz kata

--- FizzBuzz/src/FizzBuzz.java
+++ FizzBuzz/src/FizzBuzz.java
@@ -1,8 +1,5 @@
-/**
- * Created by george.theofanous on 17/09/2014.
- */
 public class FizzBuzz {
     public String print(int number) {
-        return "";
+        return null;
     }
 }
--- FizzBuzz/test/FizzBuzzTest.java
+++ FizzBuzz/test/FizzBuzzTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -5,9 +6,20 @@ import static org.hamcrest.Matchers.is;
 
 public class FizzBuzzTest {
 
+    private FizzBuzz fizzBuzz;
+
+    @Before
+    public void setUp() {
+        fizzBuzz = new FizzBuzz();
+    }
+
     @Test
-    public void printFizz() {
-        String output = new FizzBuzz().print(1);
-        assertThat(output, is("1"));
+    public void print1when1() {
+        assertThat(fizzBuzz.print(1), is("1"));
+    }
+
+    @Test
+    public void print2when2() {
+        assertThat(fizzBuzz.print(2), is("2"));
     }
 }
