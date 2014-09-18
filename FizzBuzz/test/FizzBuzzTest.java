25cdd9195858a29e07a9fda368e9474b398b53a7
michael.hatch <michael.hatch@autotrader.co.uk>
2014-09-18 17:03:03 +0100

    [michael-ha] making code easier to check out

--- FizzBuzz/test/FizzBuzzTest.java
+++ FizzBuzz/test/FizzBuzzTest.java
@@ -13,6 +13,11 @@ public class FizzBuzzTest {
         fizzBuzz = new FizzBuzz();
     }
 
+
+//##############################################################
+// STAGE ONE START
+//##############################################################
+
     @Test
     public void print2When2() {
         assertThat(fizzBuzz.print(2), is("2"));
@@ -23,53 +28,72 @@ public class FizzBuzzTest {
         assertThat(fizzBuzz.print(1), is("1"));
     }
 
-    @Test
-    public void printFizzWhen3() {
-        assertThat(fizzBuzz.print(3), is("Fizz!"));
-    }
-
-    @Test
-    public void printFizzWhen6() {
-        assertThat(fizzBuzz.print(6), is("Fizz!"));
-    }
-
-    @Test
-    public void printBuzzWhen5() {
-        assertThat(fizzBuzz.print(5), is("Buzz!"));
-    }
-
-    @Test
-    public void printBuzzWhen10() {
-        assertThat(fizzBuzz.print(10), is("Buzz!"));
-    }
-
-    @Test
-    public void printFizzBuzzWhen15() {
-        assertThat(fizzBuzz.print(15), is("FizzBuzz!"));
-    }
-
-    @Test
-    public void printBangWhen7() {
-        assertThat(fizzBuzz.print(7), is("Bang!"));
-    }
-
-    @Test
-    public void printBangWhen14() {
-        assertThat(fizzBuzz.print(14), is("Bang!"));
-    }
-
-    @Test
-    public void printFizzBangWhen21() {
-        assertThat(fizzBuzz.print(21), is("FizzBang!"));
-    }
-
-    @Test
-    public void printBuzzBangWhen35() {
-        assertThat(fizzBuzz.print(35), is("BuzzBang!"));
-    }
-
-    @Test
-    public void printFizzBuzzBangWhen105() {
-        assertThat(fizzBuzz.print(105), is("FizzBuzzBang!"));
-    }
+//##############################################################
+// STAGE TWO START
+//##############################################################
+//
+//    @Test
+//    public void printFizzWhen3() {
+//        assertThat(fizzBuzz.print(3), is("Fizz!"));
+//    }
+//
+//    @Test
+//    public void printFizzWhen6() {
+//        assertThat(fizzBuzz.print(6), is("Fizz!"));
+//    }
+//
+//##############################################################
+// STAGE THREE START
+//##############################################################
+//
+//    @Test
+//    public void printBuzzWhen5() {
+//        assertThat(fizzBuzz.print(5), is("Buzz!"));
+//    }
+//
+//    @Test
+//    public void printBuzzWhen10() {
+//        assertThat(fizzBuzz.print(10), is("Buzz!"));
+//    }
+//
+//##############################################################
+// STAGE FOUR START
+//##############################################################
+//
+//    @Test
+//    public void printFizzBuzzWhen15() {
+//        assertThat(fizzBuzz.print(15), is("FizzBuzz!"));
+//    }
+//
+//    @Test
+//    public void printBangWhen7() {
+//        assertThat(fizzBuzz.print(7), is("Bang!"));
+//    }
+//
+//##############################################################
+// STAGE FIVE START
+//##############################################################
+//
+//    @Test
+//    public void printBangWhen14() {
+//        assertThat(fizzBuzz.print(14), is("Bang!"));
+//    }
+//
+//    @Test
+//    public void printFizzBangWhen21() {
+//        assertThat(fizzBuzz.print(21), is("FizzBang!"));
+//    }
+//##############################################################
+// STAGE SIX START
+//##############################################################
+//
+//    @Test
+//    public void printBuzzBangWhen35() {
+//        assertThat(fizzBuzz.print(35), is("BuzzBang!"));
+//    }
+//
+//    @Test
+//    public void printFizzBuzzBangWhen105() {
+//        assertThat(fizzBuzz.print(105), is("FizzBuzzBang!"));
+//    }
 }
