a9ff0f888cf0d82b795e519d86517f79374d92cc
Mark Crossfield <mark.crossfield@autotrader.co.uk>
2014-11-27 17:46:32 +0000

    Removing unnecessary noise and Maven flags.

--- mvn.sh
+++ mvn.sh
@@ -2,11 +2,4 @@
 MAVEN_HOME=$PWD/tools/maven
 export MAVEN_HOME
 
-export MAVEN_OPTS=-XX:MaxPermSize=256m
-
-echo MAVEN_HOME $MAVEN_HOME
-echo M2_HOME $M2_HOME
-
-$MAVEN_HOME/bin/mvn -version
-
 $MAVEN_HOME/bin/mvn $@
