aaec05f6140f2f0446ae1b1d7505bbfb5dee71af
Mark Crossfield <mark.crossfield@autotrader.co.uk>
2014-11-27 17:54:32 +0000

    Ignoring tests that are meant to fail.

--- tennis-game/src/test/java/TennisGameTest.java
+++ tennis-game/src/test/java/TennisGameTest.java
@@ -4,28 +4,31 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 import java.io.File;
-
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class TennisGameTest {
 	
-	private File gameOne = new File("src/tennis_game/gameOne");
-	private File gameTwo = new File("src/tennis_game/gameTwo");
-	private File gameThree = new File("src/tennis_game/gameThree");
+	private File gameOne = new File("src/test/java/gameOne");
+	private File gameTwo = new File("src/test/java/gameTwo");
+	private File gameThree = new File("src/test/java/gameThree");
 	
 	@Test
+    @Ignore
 	public void gameOneTest() {
 		TennisGame game = new TennisGame(gameOne);
 		assertThat(game.playGame(), is("Player One"));
 	}
 	
 	@Test
+    @Ignore
 	public void gameTwoTest() {
 		TennisGame game = new TennisGame(gameTwo);
 		assertThat(game.playGame(), is("Player Two"));
 	}
 	
 	@Test
+    @Ignore
 	public void gameThreeTest() {
 		TennisGame game = new TennisGame(gameThree);
 		assertThat(game.playGame(), is("Draw"));
