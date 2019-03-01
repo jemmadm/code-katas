8eb2f07979457b4408898d38227a5ba0c1fe414d
Mark Crossfield <mark.crossfield@autotrader.co.uk>
2019-03-01 00:17:34 +0000

    Add boilerplate test

--- battleships/src/test/java/battleships/BattleshipsTest.java
+++ battleships/src/test/java/battleships/BattleshipsTest.java
@@ -1,4 +1,15 @@
 package battleships;
 
-public class BattleshipsTest {
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class BattleshipsTest {
+    
+    @Test
+    @Disabled
+    void firstTest() {
+        assertThat(false).isTrue();
+    }
 }
